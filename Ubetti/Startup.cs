c0a51cb91c551228366019e1f0bf470b41c20903
dvaltrick <dvaltrick@gmail.com>
2017-08-19 19:44:53 -0300

    Ubetti
    Inicio de desenvolvimento

---
+++ Ubetti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ubetti.Startup))]
+namespace Ubetti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
